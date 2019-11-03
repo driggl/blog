c76660e42e3eb9229081817fca566a87cbc66063
Sebastian Wilgosz <sebastian@driggl.com>
2019-11-03 16:09:07 +0100

    Change header size

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,51 @@
 <template>
-  <div class="container">
-    <div>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            <logo />
+  <div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <logo />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarTop"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarTop" class="navbar-menu">
+        <div class="navbar-start"></div>
+
+        <div class="navbar-end">
+          <a class="navbar-item">
+            Courses
           </a>
 
           <a
-            role="button"
-            class="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarTop"
+            href="https://patreon.com/driggl"
+            class="navbar-item"
+            target="_bank"
           >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+            Support
           </a>
         </div>
-
-        <div id="navbarTop" class="navbar-menu">
-          <div class="navbar-start"></div>
-
-          <div class="navbar-end">
-            <a class="navbar-item">
-              Courses
-            </a>
-
-            <a
-              href="https://patreon.com/driggl"
-              class="navbar-item"
-              target="_bank"
-            >
-              Support
-            </a>
-          </div>
+      </div>
+    </nav>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <h2 class="title is-4">
+            Recently on Driggl
+          </h2>
         </div>
-      </nav>
-      <section>
-        <h2 class="title is-3">
-          Recently on Driggl
-        </h2>
-      </section>
-    </div>
+      </div>
+    </section>
   </div>
 </template>
 
