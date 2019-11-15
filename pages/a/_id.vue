92665b83d9936a89867072d4460dfecd9e2bec75
Sebastian Wilgosz <sebastian@driggl.com>
2019-11-15 11:36:38 +0100

    Extract TopNav to a separate component and fix the mobile expanding

--- pages/a/_id.vue
+++ pages/a/_id.vue
@@ -1,42 +1,6 @@
 <template>
-  <div class="container">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <logo />
-        </a>
-
-        <a
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarTop"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarTop" class="navbar-menu">
-        <div class="navbar-start"></div>
-
-        <div class="navbar-end">
-          <a class="navbar-item" href="https://driggl.com/#courses">
-            Courses
-          </a>
-
-          <a
-            href="https://patreon.com/driggl"
-            class="navbar-item"
-            target="_bank"
-          >
-            Support
-          </a>
-        </div>
-      </div>
-    </nav>
+  <div>
+    <top-nav />
     <div class="container main">
       <div class="columns">
         <div class="column is-two-third-tablet">
@@ -67,13 +31,14 @@
 
 <script>
 import { mapGetters } from "vuex";
-import Logo from "~/components/Logo.vue";
+import TopNav from "~/components/organisms/TopNav";
+
 import EmailSubscriptionForm from "~/components/molecules/EmailSubscriptionForm";
 
 export default {
   components: {
     EmailSubscriptionForm,
-    Logo
+    TopNav
   },
   computed: {
     ...mapGetters("articles", ["selected"])
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,33 @@
 <template>
-  <div class="container">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <logo />
-        </a>
+  <div>
+    <top-nav />
+    <div class="container">
+      <div class="container main">
+        <div class="columns">
+          <div class="column is-two-third-tablet">
+            <section class="header">
+              <h2 class="title is-4">
+                Recently on Driggl
+              </h2>
+            </section>
 
-        <a
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarTop"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarTop" class="navbar-menu">
-        <div class="navbar-start"></div>
-
-        <div class="navbar-end">
-          <a class="navbar-item" href="https://driggl.com/#courses">
-            Courses
-          </a>
-
-          <a
-            href="https://patreon.com/driggl"
-            class="navbar-item"
-            target="_bank"
-          >
-            Support
-          </a>
+            <article-list :articles="articles" />
+          </div>
+          <aside class="column is-one-third-tablet">
+            <section class="header">
+              <h2 class="title is-4">
+                Do you like this content?
+              </h2>
+              <p>
+                <i
+                  >Join to our Newsletter for weekly updates about new articles
+                  and <strong>free programming tips!</strong></i
+                >
+              </p>
+              <email-subscription-form />
+            </section>
+          </aside>
         </div>
-      </div>
-    </nav>
-    <div class="container main">
-      <div class="columns">
-        <div class="column is-two-third-tablet">
-          <section class="header">
-            <h2 class="title is-4">
-              Recently on Driggl
-            </h2>
-          </section>
-
-          <article-list :articles="articles" />
-        </div>
-        <aside class="column is-one-third-tablet">
-          <section class="header">
-            <h2 class="title is-4">
-              Do you like this content?
-            </h2>
-            <p>
-              <i
-                >Join to our Newsletter for weekly updates about new articles
-                and <strong>free programming tips!</strong></i
-              >
-            </p>
-            <email-subscription-form />
-          </section>
-        </aside>
       </div>
     </div>
   </div>
@@ -69,15 +35,17 @@
 
 <script>
 import { mapGetters } from "vuex";
-import Logo from "~/components/Logo.vue";
 import ArticleList from "~/components/organisms/ArticleList";
 import EmailSubscriptionForm from "~/components/molecules/EmailSubscriptionForm";
+import Logo from "~/components/Logo.vue";
+import TopNav from "~/components/organisms/TopNav";
 
 export default {
   components: {
     ArticleList,
     EmailSubscriptionForm,
-    Logo
+    Logo,
+    TopNav
   },
   computed: {
     ...mapGetters("articles", ["articles"])
