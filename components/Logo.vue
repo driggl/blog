a5606a4d787b60a2cf40a411f1cb0ff661a4716c
Sebastian Wilgosz <sebastian@driggl.com>
2020-02-22 15:08:18 +0100

    Change logo to not use image but background-image css rule
    Reason: This is an attempt to solve https://stackoverflow.com/q/60341816/2493018

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,3 +1,13 @@
 <template>
-  <img src="/logo-small.png" />
+  <div class="logo"></div>
 </template>
+
+<style scoped>
+.logo {
+  width: 83px;
+  height: 28px;
+  display: block;
+  background-image: url("/logo-small.png");
+  background-size: 83px 28px;
+}
+</style>
