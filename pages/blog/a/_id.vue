1d9f8fc532eb2fe75583c1f0f957965232d2f09b
Sebastian Wilgosz <sebastian@driggl.com>
2020-02-23 18:23:20 +0100

    Comment out the hardcoded image thumbnail

--- pages/blog/a/_id.vue
+++ pages/blog/a/_id.vue
@@ -7,9 +7,9 @@
         'background-image': 'url(' + selected.attributes.thumbnail.full + ')'
       }"
     >
-      <div class="cover social">
+      <!-- <div class="cover social">
         <img :src="selected.attributes.thumbnail['sharing-square']" />
-      </div>
+      </div> -->
       <div class="hero-body">
         <div class="container has-text-centered">
           <h1 class="title">
