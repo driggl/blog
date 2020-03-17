5ecd352a41ac00f8b07da6401b29a1d537ea94a6
Sebastian Wilgosz <sebastian@driggl.com>
2020-03-17 16:21:46 +0100

    Fix the OM account setup

--- plugins/optinmonster.js
+++ plugins/optinmonster.js
@@ -4,8 +4,8 @@ import Router from 'vue-router'
 const router = new Router ({ })
 
 router.beforeEach((to, from, next) => {
-  if (window.om82043_) {
-    window.om5678_72987.reset()
+  if (window.om82043_72987) {
+    window.om82043_72987.reset()
   }
 })
 
