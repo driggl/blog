243d956448a708e18c8b4daee0af24fd15f495ff
lusarz <lukasz.usarz@gmail.com>
2020-10-21 09:44:18 +0200

    Fix: use custom built vue-infinite-loading

--- plugins/infinite-loading.js
+++ plugins/infinite-loading.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import InfiniteLoading from 'vue-infinite-loading'
+import InfiniteLoading from '../vendor/vue-infinite-loading'
 import DglLoader from '@/components/atoms/DglLoader'
 
 
